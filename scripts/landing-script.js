1b1945367e08f17de18b42531596f5a15f747961
Timothy <vocalync@gmail.com>
2024-10-12 15:25:28 +0100

    resolved favicon manifest problems

--- scripts/landing-script.js
+++ scripts/landing-script.js
@@ -5,24 +5,24 @@
 const faviconApple = document.createElement("link");
 faviconApple.setAttribute("rel", "apple-touch-icon");
 faviconApple.setAttribute("sizes", "180x180");
-faviconApple.setAttribute("href", "./favicon/apple-touch-icon.png");
+faviconApple.setAttribute("href", "./apple-touch-icon.png");
 document.head.appendChild(faviconApple);
 
 const faviconIcon = document.createElement("link");
 faviconIcon.setAttribute("rel", "icon");
 faviconIcon.setAttribute("type", "image/png");
 faviconIcon.setAttribute("sizes", "32x32");
-faviconIcon.setAttribute("href", "./favicon/favicon-32x32.png");
+faviconIcon.setAttribute("href", "./favicon-32x32.png");
 document.head.appendChild(faviconIcon);
 
 const faviconIcon16 = document.createElement("link");
 faviconIcon16.setAttribute("rel", "icon");
 faviconIcon16.setAttribute("type", "image/png");
 faviconIcon16.setAttribute("sizes", "16x16");
-faviconIcon16.setAttribute("href", "./favicon/favicon-16x16.png");
+faviconIcon16.setAttribute("href", ".favicon-16x16.png");
 document.head.appendChild(faviconIcon16);
 
 const faviconManifest = document.createElement("link");
 faviconManifest.setAttribute("rel", "manifest");
-faviconManifest.setAttribute("href", "./favicon/site.webmanifest");
+faviconManifest.setAttribute("href", "./site.webmanifest");
 document.head.appendChild(faviconManifest);
