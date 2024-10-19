10607b105d4ccca2de965c5881cae10aea00e4e0
Timothy <vocalync@gmail.com>
2024-10-19 22:38:13 +0100

    Added dark mode control and coming soon pages

--- script.js
+++ script.js
@@ -30,6 +30,25 @@ nav.innerHTML = `
         </a>
       </div>
       <div class="nav__items">
+      <div id="mode">
+            <!-- system -->
+            <svg id="system" viewBox="0 0 15 15" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" clip-rule="evenodd"
+                d="M7.49991 0.876892C3.84222 0.876892 0.877075 3.84204 0.877075 7.49972C0.877075 11.1574 3.84222 14.1226 7.49991 14.1226C11.1576 14.1226 14.1227 11.1574 14.1227 7.49972C14.1227 3.84204 11.1576 0.876892 7.49991 0.876892ZM7.49988 1.82689C4.36688 1.8269 1.82707 4.36672 1.82707 7.49972C1.82707 10.6327 4.36688 13.1725 7.49988 13.1726V1.82689Z" />
+            </svg>
+            <!-- moon -->
+            <svg id="moon" style="display: none;" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M12 22C17.5228 22 22 17.5228 22 12C22 11.5373 21.3065 11.4608 21.0672 11.8568C19.9289 13.7406 17.8615 15 15.5 15C11.9101 15 9 12.0899 9 8.5C9 6.13845 10.2594 4.07105 12.1432 2.93276C12.5392 2.69347 12.4627 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" />
+            </svg>
+
+            <!-- sun -->
+            <svg id="sun" style="display: none;" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 24 24">
+              <path
+                d="M 12 0 C 11.4 0 11 0.4 11 1 L 11 2 C 11 2.6 11.4 3 12 3 C 12.6 3 13 2.6 13 2 L 13 1 C 13 0.4 12.6 0 12 0 z M 4.1992188 3.1992188 C 3.9492188 3.1992187 3.7 3.3 3.5 3.5 C 3.1 3.9 3.1 4.5003906 3.5 4.9003906 L 4.1992188 5.5996094 C 4.5992187 5.9996094 5.1996094 5.9996094 5.5996094 5.5996094 C 5.9996094 5.1996094 5.9996094 4.5992188 5.5996094 4.1992188 L 4.9003906 3.5 C 4.7003906 3.3 4.4492188 3.1992188 4.1992188 3.1992188 z M 19.800781 3.1992188 C 19.550781 3.1992188 19.299609 3.3 19.099609 3.5 L 18.400391 4.1992188 C 18.000391 4.5992187 18.000391 5.1996094 18.400391 5.5996094 C 18.800391 5.9996094 19.400781 5.9996094 19.800781 5.5996094 L 20.5 4.9003906 C 20.9 4.5003906 20.9 3.9 20.5 3.5 C 20.3 3.3 20.050781 3.1992188 19.800781 3.1992188 z M 12 5 A 7 7 0 0 0 5 12 A 7 7 0 0 0 12 19 A 7 7 0 0 0 19 12 A 7 7 0 0 0 12 5 z M 1 11 C 0.4 11 0 11.4 0 12 C 0 12.6 0.4 13 1 13 L 2 13 C 2.6 13 3 12.6 3 12 C 3 11.4 2.6 11 2 11 L 1 11 z M 22 11 C 21.4 11 21 11.4 21 12 C 21 12.6 21.4 13 22 13 L 23 13 C 23.6 13 24 12.6 24 12 C 24 11.4 23.6 11 23 11 L 22 11 z M 4.9003906 18.099609 C 4.6503906 18.099609 4.3992188 18.200391 4.1992188 18.400391 L 3.5 19.099609 C 3.1 19.499609 3.1 20.1 3.5 20.5 C 3.9 20.9 4.5003906 20.9 4.9003906 20.5 L 5.5996094 19.800781 C 5.9996094 19.400781 5.9996094 18.800391 5.5996094 18.400391 C 5.3996094 18.200391 5.1503906 18.099609 4.9003906 18.099609 z M 19.099609 18.099609 C 18.849609 18.099609 18.600391 18.200391 18.400391 18.400391 C 18.000391 18.800391 18.000391 19.400781 18.400391 19.800781 L 19.099609 20.5 C 19.499609 20.9 20.1 20.9 20.5 20.5 C 20.9 20.1 20.9 19.499609 20.5 19.099609 L 19.800781 18.400391 C 19.600781 18.200391 19.349609 18.099609 19.099609 18.099609 z M 12 21 C 11.4 21 11 21.4 11 22 L 11 23 C 11 23.6 11.4 24 12 24 C 12.6 24 13 23.6 13 23 L 13 22 C 13 21.4 12.6 21 12 21 z">
+              </path>
+            </svg>
+            </div>
         <div class="hamburger">
           <span class="bar"></span>
           <span class="bar"></span>
@@ -188,3 +207,39 @@ faviconManifest.setAttribute("rel", "manifest");
 faviconManifest.setAttribute("href", "./site.webmanifest");
 document.head.appendChild(faviconManifest);
 
+
+
+// Light mode control
+
+document.getElementById('mode').addEventListener('click', function () {
+  const html = document.documentElement;
+  const systemIcon = document.getElementById('system');
+  const sunIcon = document.getElementById('sun');
+  const moonIcon = document.getElementById('moon');
+
+  if (html.classList.contains('light-mode')) {
+    // Switch to dark mode
+    html.classList.remove('light-mode');
+    html.classList.add('dark-mode');
+
+    sunIcon.style.display = 'none';
+    moonIcon.style.display = 'block';
+    systemIcon.style.display = 'none';
+  } else if (html.classList.contains('dark-mode')) {
+    // Switch to system mode
+    html.classList.remove('dark-mode');
+    html.classList.add('system-mode');
+
+    sunIcon.style.display = 'none';
+    moonIcon.style.display = 'none';
+    systemIcon.style.display = 'block';
+  } else {
+    // Switch to light mode
+    html.classList.remove('system-mode');
+    html.classList.add('light-mode');
+
+    sunIcon.style.display = 'block';
+    moonIcon.style.display = 'none';
+    systemIcon.style.display = 'none';
+  }
+});
